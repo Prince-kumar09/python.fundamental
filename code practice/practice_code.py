d918446a8f2f59fee382adf943e0166f1b8b9c76
Prince kumar <pkrana.com>
2026-07-28 21:23:30 +0530

    question based on operator precedence

--- code practice/practice_code.py
+++ code practice/practice_code.py
@@ -1,3 +1,30 @@
 #question based on operator precedence
 x=5+2*3
 print(x)
+
+#Question 2
+
+x=(5+2)*3
+print(x)
+
+#Question 3
+s=2**4**4
+print(s)
+
+#question 4
+x=10-4-3
+print(x)
+
+#question 5
+x=4+6/2
+print(x)
+
+#question 6
+x=8//3+2
+print(x)
+
+#question 7
+x=3+4*2>10
+print(x)
+
+
