3150f3fb0de4902234adc2f07425576dd7598f85
Prince kumar <pkrana.com>
2026-07-28 23:41:24 +0530

    adding some problem

---
+++ code practice/chara.py
@@ -0,0 +1,9 @@
+word=input("enter the word ")
+for ch in word:
+    print(ch)
+    letter="word"
+
+    if (letter in word):
+        print("letter exist")
+        
+    
---
+++ code practice/loop.py
@@ -0,0 +1,12 @@
+#print 1 to 5
+i=1
+while i<=5:
+    print(i)
+    i+=1
+
+    
+#print 5 to 1
+i=5
+while i>0:
+    print(i)
+    i-=1
