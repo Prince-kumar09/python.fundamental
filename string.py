af5a193e6f69c0d9d02a006d91a46c2e1d04aee5
Prince kumar <pkrana0906@gmail.com>
2026-08-06 22:31:25 +0530

    practice string concept

---
+++ string.py
@@ -0,0 +1,72 @@
+name="prince"
+city="gkp"
+a='python'
+b="""prince 
+kumar"""
+print(name)
+print(city)
+print(a)
+print(b)
+#string slicing
+print(name[0])
+print(name[1])
+print(name[2])
+word="python"
+print(word[-1])
+print(word[-2])
+#string length
+print(len(name))
+#string slicing
+print(name[0:3])
+print(word[:3])
+print(word[3:])
+print(word[-4:])
+print(word[::2])
+print(word[::-2])
+#string concatenation
+first="prince"
+last="kumar"
+print(first+" "+last)
+print(first+"_"+last)
+#string repetition
+print("hi"*3)
+#membership operator
+text="prince"
+print("p" in text)
+print("z" in text)
+print("z" not in text)
+#escape character
+print("hello\nworld")
+#tab space
+print("hello\tworld")
+#double quotes
+print("my name is \"prince\"")
+#backslash
+print("c:\\users\\prince")
+#imp string method
+#upper case
+name="prince"
+print(name.upper())
+#lower case
+print("python".lower())
+#title case
+print("python programming".title())
+#capitalize
+print("python".capitalize)
+#strip
+name="prince"
+print(name.strip())
+#replace
+text="i like java"
+print(text.replace("java","python"))
+#count
+text="banana"
+print(text.count(a))
+#find
+text="programming"
+print(text.find("g"))
+#start with
+print("python".startswith("py"))
+#endwith
+print("python".endswith("on"))
+
