5bd9c19698e0f622489345ffae179e49306450aa
Prince kumar <pkrana0906@gmail.com>
2026-08-06 23:18:26 +0530

    some codding challenges

---
+++ string_practice.py
@@ -0,0 +1,8 @@
+#coding challenges
+name=input("Enter name:")
+print("lenght:",len(name))
+print("upper:",name.upper())
+print("lower:",name.lower())
+print("reverse:",name[::-1])
+print("first:",name[0])
+print("last:",name[-1])
