9dfa3abc529a4f678e916daaf50323b5b657cf4e
Prince kumar <pkrana0906@gmail.com>
2026-08-15 23:45:22 +0530

    practice string

--- code practice/string.py
+++ code practice/string.py
@@ -25,3 +25,34 @@ text.endswith("Python")         # False
 print(text.startswith("Python"))
 print(text.endswith("Python"))
 
+#imp concept split method
+text = "Python is easy"
+
+words = text.split()
+
+print(words)
+
+print(type(words))
+
+text = "I love Python"
+text.find("Python")     # 7
+text.count("Python")    # 1
+text.split()            # ['I', 'love', 'Python']
+
+words = "I love Python".split()
+
+print(words[1])
+
+
+words = ["I", "love", "Python"]
+
+result = " ".join(words)
+
+print(result)
+
+
+fruits = ["apple", "banana", "mango"]
+
+result = ",".join(fruits)
+
+print(result)
