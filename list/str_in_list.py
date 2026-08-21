24f3fe20e0a5ac34765037ffcc82187e8a94e02b
Prince kumar <pkrana0906@gmail.com>
2026-08-21 20:57:39 +0530

    practice some question based on list

---
+++ list/str_in_list.py
@@ -0,0 +1,13 @@
+numbers = [10, 20]
+
+numbers.extend("ABC")# Python string ko characters ka collection maanta hai.
+
+print(numbers)
+
+
+
+num = [10, 20]
+
+num.append("ABC")# add only one element
+
+print(num)
