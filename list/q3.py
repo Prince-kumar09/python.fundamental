b852bc4be83ffc97d252f66b56ac9ab49bf814e4
Prince kumar <pkrana0906@gmail.com>
2026-08-21 20:50:40 +0530

    practice list

---
+++ list/q3.py
@@ -0,0 +1,36 @@
+numbers=[10,20,30] #adding 40 at the end of the list
+numbers.append(40)
+print(numbers)
+
+
+
+#insert 15 at index 1
+num=[10,20,30]
+num.insert(1,15)
+print(num)
+
+
+
+##adding another list
+numbers.extend([10,20,30])
+print(numbers)
+
+#remove value from the list
+numb=[10,20,30,40]
+numb.remove(30)
+print(numb)
+
+##remove element of index 1
+nu=[10,20,30,40]
+nu.pop(1)
+print(nu)
+
+#remove last element from the list
+digit=[10,20,30,40,50]
+digit.pop()
+print(digit)
+
+#empty the list
+dig=[10,20,30]
+dig.clear()
+print(dig)
