8cc145671e0326748a5388a1fbb2cd66272540a2
Prince kumar <pkrana0906@gmail.com>
2026-08-22 22:18:11 +0530

    practice list

---
+++ list/listq4.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30, 40, 50, 30, 40, 30,]
+print(numbers.index(30))
+print(numbers.count(30))
+
+fruits=["mango", "banana", "apple", "banana"]
+print(fruits.index("banana"))
+print(fruits.count("banana"))
