f93e5015285922c1f69c9d6cb091afe8785f4033
Prince kumar <pkrana0906@gmail.com>
2026-08-23 20:17:53 +0530

    practice list

---
+++ list/nested_list.py
@@ -0,0 +1,16 @@
+students= [
+    ["prince",20],
+    ["aman",21],
+    ["rahul",19]
+]
+print(students[1][0])
+print(students[2][1])
+
+#task3
+numbers=[
+    [10,20,30],
+    [40,50,60],
+    [70,80,90]
+]
+print(numbers[1][2])
+print(numbers[2][0])
