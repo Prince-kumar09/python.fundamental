d12a6adc82e516931156d84c42120a422ca97635
Prince kumar <pkrana0906@gmail.com>
2026-08-23 21:02:08 +0530

    practice list

---
+++ list/in_notin_list.py
@@ -0,0 +1,26 @@
+# it return the boolen value 
+numbers=[10,20,30,40,50]
+print(30 in numbers)
+print(100 not in numbers)
+
+fruits=["apple", "mango", "banana", "orange"]
+if "banana" in fruits:
+    print("avilabl:")
+else:
+    print("not avilable:")
+
+
+
+numbers = [10, 20, 30]
+
+print(20 in numbers)
+print(50 in numbers)
+print(50 not in numbers)    
+
+
+numbers = [
+    [10, 20],
+    [30, 40],
+    [50, 60]
+]
+print(40 in numbers[1])
