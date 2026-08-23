6ff5f86fdc002f0c9ca1be469dae1eb507b7bc6c
Prince kumar <pkrana0906@gmail.com>
2026-08-23 20:42:45 +0530

    list practice some basic problem

--- list/nested_list.py
+++ list/nested_list.py
@@ -14,3 +14,49 @@ numbers=[
 ]
 print(numbers[1][2])
 print(numbers[2][0])
+# how to modify nested list 
+
+students= [
+    ["prince",20],
+    ["aman",21],
+    ["rahul",19]
+]
+students[1][1]=22
+print(students)
+students.append(["rahul",19])
+print(students)
+
+# print all the value in different row
+numbers = [
+    [10, 20],
+    [30, 40],
+    [50, 60]
+]
+for x in numbers:
+    for y in x:
+        print(y)
+
+#print sum of all the element or numbers
+numbers = [
+    [10, 20],
+    [30, 40],
+    [50, 60]
+]
+total=0
+for x in numbers:
+    for y in x:
+        total=total+y
+print(total)
+
+# print even number of the given list
+
+numbers = [
+    [10, 20],
+    [30, 40],
+    [50, 60]
+]
+
+for x in numbers:
+    for y in x:
+        if y%2==0:
+            print(y)
