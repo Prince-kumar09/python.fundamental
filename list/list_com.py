3cebf6fdb2813f972b8d1fbc76d95e86d2a52b1d
Prince kumar <pkrana0906@gmail.com>
2026-08-24 20:47:07 +0530

    practice list

--- list/list_com.py
+++ list/list_com.py
@@ -3,3 +3,11 @@ numbers = [10, 20, 30, 40]  #[ kya banana hai   for   kis list se banana hai ]
 new_numbers = [x + 10 for x in numbers]  #[expression for variable in list]
 
 print(new_numbers)
+
+
+ #another question
+fruits = ["apple", "mango", "banana"]
+
+upper_fruits = [x.upper() for x in fruits]
+
+print(upper_fruits)
