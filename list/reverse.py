d8b12f199e0a73c4b9424d5868212254fdbbe45c
Prince kumar <pkrana0906@gmail.com>
2026-08-26 12:05:05 +0530

    practice some list question

---
+++ list/reverse.py
@@ -0,0 +1,12 @@
+#1️⃣ reverse() — sirf order ulta karta hai
+numbers = [10, 20, 30, 40, 50]
+
+numbers.reverse()
+
+print(numbers)
+#3️⃣ sort(reverse=True) — descending order mein sort karta hai
+numbers = [30, 10, 50, 20, 40]
+
+numbers.sort(reverse=True)# short in decending order
+
+print(numbers)
