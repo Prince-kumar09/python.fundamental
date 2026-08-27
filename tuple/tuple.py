8d2a5ad2847bcbea7cdd8dc6557424556d4ddbb6
Prince kumar <pkrana0906@gmail.com>
2026-08-27 21:16:30 +0530

    practice list

--- tuple/tuple.py
+++ tuple/tuple.py
@@ -11,3 +11,10 @@ numbers = [10, 20, 30]
 numbers[0] = 100
 
 print(numbers)
+#LIST = Change allowed 🔄
+#TUPLE = Change NOT allowed 🔒
+
+a = (10)
+b = (10,)
+print(type(a))#<class 'int'>
+print(type(b))#<class 'tuple'>
