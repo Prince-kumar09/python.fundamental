26167b5269701b5b4a4c79db4c782fac5a719f79
Prince kumar <pkrana0906@gmail.com>
2026-08-27 21:34:20 +0530

    practice list

---
+++ tuple/tuple4.py
@@ -0,0 +1,15 @@
+# tuple repetation
+numbers = (10, 20)
+
+result = numbers * 3
+
+print(result)
+'''Yahan thoda sa correction 👍 * Tuple ke elements ko multiply nahi karta.
+
+Tuple mein * 3 ka matlab hai poore tuple ko 3 times repeat karna.'''
+#imp question based on list and tuple
+a = (10, 20, [30, 40])
+
+a[2].append(50)
+
+print(a)
