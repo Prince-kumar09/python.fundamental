2d18dd6d81416056b2206df3939175ea0ab32d65
Prince kumar <pkrana0906@gmail.com>
2026-08-27 21:19:44 +0530

    practice list unpacked

---
+++ tuple/tuple2.py
@@ -0,0 +1,15 @@
+a = ()
+b = (10,)
+c = (10, 20)
+
+print(type(a))# tuple
+print(type(b))# tuple
+print(type(c))# tuple
+
+
+
+numbers = 10, 20, 30#Ismein brackets nahi hain, phir bhi Python ise tuple banata hai.
+#Is concept ko Tuple Packing kehte hain.
+
+print(numbers)
+print(type(numbers))
