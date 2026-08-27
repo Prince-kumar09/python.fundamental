85edccefba81ed932ec7b9838ffe7ca692e7e3fd
Prince kumar <pkrana0906@gmail.com>
2026-08-27 21:24:55 +0530

    practice tuple in python

---
+++ tuple/tuple3.py
@@ -0,0 +1,19 @@
+numbers = (10, 20, 30)
+
+a, b, c = numbers
+
+print(a)
+print(b)
+print(c)
+
+'''numbers = (10, 20, 30)
+
+a, b = numbers
+# value error'''
+#concatenation in tuple
+a = (10, 20, 30)
+b = (40, 50)
+
+c = a + b
+
+print(c)
