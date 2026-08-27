ed27961797999053dc6d4f2971dd078c0a69bd0f
Prince kumar <pkrana0906@gmail.com>
2026-08-27 21:11:59 +0530

    practice list

---
+++ tuple.py
@@ -0,0 +1,13 @@
+numbers = (10, 20, 30, 40)
+
+print(numbers)
+'''numbers = (10, 20, 30)
+
+numbers[0] = 100  #error aayega
+'''
+# in list 
+numbers = [10, 20, 30]
+
+numbers[0] = 100
+
+print(numbers)
