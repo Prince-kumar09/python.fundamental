7a681138d65f9f407d61410508c1ac0961e6de11
Prince kumar <pkrana0906@gmail.com>
2026-08-28 22:19:13 +0530

    practice tuple

---
+++ tuple/tuple9.py
@@ -0,0 +1,24 @@
+a = (1, 2, 3)
+b = (1, 2, 3)
+
+print(a == b)#true
+#(1, 2, 3) == (1, 2, 3) → True ✅
+#(1, 2, 3) == (1, 3, 2) → False ❌
+
+'''numbers = (10, 20, 30, 40)
+
+del numbers[2]
+
+print(numbers)'''
+#error
+
+
+
+
+
+
+
+numbers = (10, 20, 30, 40)
+
+del numbers
+#Ye possible hai — isse poora Tuple variable delete ho jayega.
