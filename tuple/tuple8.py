4f1edb26efcd4b8d525df22dba98e741ac928758
Prince kumar <pkrana0906@gmail.com>
2026-08-28 22:09:42 +0530

    practice tuple

---
+++ tuple/tuple8.py
@@ -0,0 +1,7 @@
+numbers = (1, 2, 3)
+
+print(numbers * 2)
+print(tuple(x * 2 for x in numbers))
+# both concept is very tricky try to understand and learn this things 
+#tuple * 2 → repeat 🔁
+#x * 2 → each element multiply ✖️
