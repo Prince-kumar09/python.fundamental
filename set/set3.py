183972902185df3fa4ce0f9b1e75fd5b49f1c078
Prince kumar <pkrana0906@gmail.com>
2026-08-30 13:29:47 +0530

    Find difference between two lists using sets

---
+++ set/set3.py
@@ -0,0 +1,6 @@
+list1 = [10, 20, 30]
+list2 = [30, 40, 50]
+set1=set(list1)
+set2=set(list2)
+result=set1.union(set2)
+print(result)
---
+++ set/set4.py
@@ -0,0 +1,6 @@
+list1 = [10, 20, 30, 40, 50]
+list2 = [30, 40, 60, 70]
+set1=set(list1)
+set2=set(list2)
+result=set1.difference(set2)
+print(result)
