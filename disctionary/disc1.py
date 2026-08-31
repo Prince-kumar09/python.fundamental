23929d5ce824cb6a977e062f86cbdce4e370afe7
Prince kumar <pkrana0906@gmail.com>
2026-08-31 21:50:06 +0530

    disctionary creation and key based access

---
+++ disctionary/disc1.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Prince",
+    "age": 20,
+    "marks": 90,
+    "branch": "CSE"
+}
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Marks:", student["marks"])
+print("Branch:", student["branch"])
