1e7a168d78ac72b880b7dc3060e155264fe0bb3e
Prince kumar <pkrana0906@gmail.com>
2026-08-31 21:53:42 +0530

    remove the key and value from disc and return the value of the key

---
+++ disctionary/disc4.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Prince",
+    "age": 20,
+    "marks": 90
+}
+
+removed_marks = student.pop("marks")
+
+print("Removed Marks:", removed_marks)
+print("Updated Student:", student)
