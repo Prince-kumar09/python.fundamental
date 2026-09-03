ec62c7e50748a11d394ac05f51f6e10ca1d3ba28
Prince kumar <pkrana0906@gmail.com>
2026-09-03 21:37:50 +0530

    practice nested dictionary

---
+++ dictionary/disc16.py
@@ -0,0 +1,15 @@
+students = {
+    "student1": {
+        "name": "Prince",
+        "marks": 90
+    },
+    "student2": {
+        "name": "Aman",
+        "marks": 75
+    }
+}
+
+students["student2"]["marks"] = 85
+students["student1"]["city"] = "Lucknow"
+
+print(students)
