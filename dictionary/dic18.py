0c93bb3427b1c4a37bdaae309c164dc9e6b69fbc
Prince kumar <pkrana0906@gmail.com>
2026-09-04 21:57:51 +0530

    nested dictionary plus condition

---
+++ dictionary/dic18.py
@@ -0,0 +1,18 @@
+students = {
+    "student1": {
+        "name": "Prince",
+        "marks": 90
+    },
+    "student2": {
+        "name": "Aman",
+        "marks": 75
+    },
+    "student3": {
+        "name": "Rahul",
+        "marks": 85
+    }
+}
+
+for student, data in students.items():
+    if data["marks"] >= 80:
+        print(data["name"])
