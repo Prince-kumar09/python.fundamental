38bf92b51178a4d5867f58357733e3e9849e40f0
Prince kumar <pkrana0906@gmail.com>
2026-09-05 19:40:51 +0530

    student summary in function

---
+++ function/basic.py
@@ -0,0 +1,16 @@
+
+def show_result():
+    name = "Prince"
+    marks = 85
+
+    print("Student Name:", name)
+    print("Marks:", marks)
+
+    if marks >= 40:
+        print("Result: Pass")
+    else:
+        print("Result: Fail")
+
+
+show_result()
+
