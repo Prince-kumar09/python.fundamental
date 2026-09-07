2c6a88ebad7777928758587ef8021b0cfb7a7f0d
Prince kumar <pkrana0906@gmail.com>
2026-09-07 20:38:59 +0530

    practice function

--- function/func6.py
+++ function/func6.py
@@ -3,3 +3,9 @@ def student(name,age,marks):
     print("age:",age)
     print("marks:",marks)
 student('prince',20,85)
+
+def employee(name,department,salary):
+    print("name:",name)
+    print("department:",department)
+    print("salary:",salary)
+employee('prince','aiml',50000)
