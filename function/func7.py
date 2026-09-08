7896e916966f3fdbe7a603c164b70437fb8e5b14
Prince kumar <pkrana0906@gmail.com>
2026-09-08 21:35:13 +0530

    practice function

---
+++ function/func7.py
@@ -0,0 +1,6 @@
+def order(customer,item,quantity,price):
+    total=quantity*price
+    print("customer",customer)
+    print("item",item)
+    print("total",total)
+order('prince',"keyboard",2,1500)
