9bcc074848e36e58d3f061504253cccb76f81f94
Prince kumar <pkrana0906@gmail.com>
2026-09-11 21:35:22 +0530

    practice addition subtraction and product using return keyword

---
+++ function/func14.py
@@ -0,0 +1,7 @@
+def calculate(a,b):
+    return a+b, a-b ,a*b
+addition ,subtraction ,product = calculate(10,5)
+
+print("addition:",addition)
+print("subtraction:",subtraction)
+print("product:",product)
