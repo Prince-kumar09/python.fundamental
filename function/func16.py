c18b2ac6ad853592227f1585dbf0bcfd82368342
Prince kumar <pkrana0906@gmail.com>
2026-09-11 21:57:45 +0530

    practice function with multile parameter to solve with using return keyword

---
+++ function/func16.py
@@ -0,0 +1,19 @@
+def add_three(a,b,c):
+    return a+b+c
+print(add_three(10,20,30))
+
+
+
+def student_info(name,age,branch):
+    return name,age,branch
+print(student_info("prince",19,"ai/ml"))
+
+
+
+def rectangle(lenght,width):
+    area=lenght*width
+    parameter=2*(lenght+width)
+    return area,parameter
+print(rectangle(10,20))
+
+
