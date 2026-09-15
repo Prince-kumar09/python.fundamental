2b3047355f834b0fd2ef82d592ad4e123d811625
Prince kumar <pkrana0906@gmail.com>
2026-09-15 21:36:15 +0530

    practice func with args and kwargs

--- function/func20.py
+++ function/func20.py
@@ -2,3 +2,15 @@ def student(**kwargs):
     for key,value in kwargs.items():
         print(key,":",value)
 student(name="prince",age=19,branch="ai/ml",college="itm")
+
+
+
+
+
+def student(*args, **kwargs):
+    for x in args:
+        print(x)
+    for key , value in kwargs.items():
+        print(key,":", value)   
+        
+student("python","ai","ml",name="prince",age=19) 
