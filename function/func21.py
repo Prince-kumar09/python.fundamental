d2076ffb2cb7b7e2b768b051d82641386ac10e14
Prince kumar <pkrana0906@gmail.com>
2026-09-18 22:02:30 +0530

    practice function with nested condition

--- function/func21.py
+++ function/func21.py
@@ -3,3 +3,14 @@ def outer():
         print("hellow prince")
     inner()
 outer()
+
+
+
+def calculator(a, b):
+
+    def add():
+        return a + b
+
+    return add()
+
+print(calculator(10, 20))
