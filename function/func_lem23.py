ca44cb60e6a9546970e658777d7fcec338477881
Prince kumar <pkrana0906@gmail.com>
2026-09-19 20:58:30 +0530

    practice function with lambda function

---
+++ function/func_lem23.py
@@ -0,0 +1,14 @@
+sqr=lambda x: x*x
+print(sqr(6))
+
+add =lambda a,b:a+b
+print(add(2,3))
+
+
+cheak=lambda n:"even" if n%2==0 else"odd"
+print(cheak(23))
+
+
+maximum = lambda a, b: a if a > b else b
+
+print(maximum(2, 3))
