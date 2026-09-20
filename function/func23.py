4f98a80dc2e32503d225e52679abcd207e1abd39
Prince kumar <pkrana0906@gmail.com>
2026-09-20 22:07:25 +0530

    practice function using map function

--- function/func23.py
+++ function/func23.py
@@ -27,3 +27,12 @@ numbers = [1, 2, 3, 4, 5]
 result = map(lambda x: x * 5, numbers)
 
 print(list(result))
+
+
+
+
+numbers = [10, 20, 30, 40]
+
+result = map(lambda x: str(x), numbers)
+
+print(list(result))
