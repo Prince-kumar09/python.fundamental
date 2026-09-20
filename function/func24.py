26b95c7aca949670dcc64c28acd25d13c83e4d8d
Prince kumar <pkrana0906@gmail.com>
2026-09-20 22:09:54 +0530

    practice function using filter

--- function/func24.py
+++ function/func24.py
@@ -3,3 +3,10 @@ numbers = [1, 2, 3, 4, 5, 6]
 result = filter(lambda x: x % 2 == 0, numbers)
 
 print(list(result))
+
+
+numbers = [10, 15, 20, 25, 30, 35]
+
+result = filter(lambda x: x % 2 == 0, numbers)
+
+print(list(result))
