a27ab53f5cd57d49f210f65a88c22d2afee44d5a
Prince kumar <pkrana0906@gmail.com>
2026-09-20 22:05:09 +0530

    praction

--- function/func23.py
+++ function/func23.py
@@ -10,3 +10,12 @@ numbers = [2, 4, 6, 8, 10]
 result = map(lambda x: x * 2, numbers)
 
 print(list(result))
+
+
+
+
+numbers = [1, 2, 3, 4, 5]
+
+result = map(lambda x: x * x, numbers)
+
+print(list(result))
