a8acee16a78b282a56d79d9c3bea9f1efde9f367
Prince kumar <pkrana0906@gmail.com>
2026-09-22 23:02:42 +0530

    practice function

--- function/func24.py
+++ function/func24.py
@@ -10,3 +10,26 @@ numbers = [10, 15, 20, 25, 30, 35]
 result = filter(lambda x: x % 2 == 0, numbers)
 
 print(list(result))
+
+
+
+
+
+def count_characters(name):
+    return len(name)
+
+print(count_characters("Prince"))
+
+
+
+
+def count_vowels(text):
+    count = 0
+
+    for x in text:
+        if x in "aeiou":
+            count += 1
+
+    return count
+
+print(count_vowels("education"))
