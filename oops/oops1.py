6cd4b295a2a83423b44f1096bd2f275ae3ff8076
Prince kumar <pkrana0906@gmail.com>
2026-09-23 11:47:33 +0530

    practice oops concept of instance of method

---
+++ oops/oops1.py
@@ -0,0 +1,62 @@
+#instance methods
+
+class Student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def introduce(self):
+        print("my name is",self.name)
+        print("my age is ",self.age)
+student1=Student("prince",21)
+student1.introduce()
+
+
+
+
+class Rectangle:
+    def __init__(self,lenght,width):
+        self.length=lenght
+        self.width=width
+    def area(self):
+        return self.length*self.width
+r1=Rectangle(10,5)
+print(r1.area())
+
+
+
+
+class Rectangle:
+
+    def __init__(self, length, width):
+        self.length=length
+        self.width=width
+
+    def area(self):
+        return self.length*self.width
+
+    def perimeter(self):
+        return 2 *(self.length+self.width)
+
+
+r1 = Rectangle(10, 5)
+print(r1.area())
+print(r1.perimeter())
+
+
+
+
+
+class Student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def result(self):
+        if self.marks>=40:
+            print("pass")
+        else:
+            print("fail")
+student1=Student("prince ",75)
+student2=Student("rahul",35)
+
+student1.result()
+student2.result()
